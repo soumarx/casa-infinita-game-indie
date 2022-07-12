e3ddf14fa61fbb329b192152d262403659e0fc92
soumarx@protonmail.com <&RN@Ix55hr^$rzGsC5vz>
2022-07-11 21:32:47 -0300

    Primeira subida

---
+++ eventos.py
@@ -0,0 +1,95 @@
+import time
+import play
+
+
+def introducao():
+    print("Baseado na Creepypasta Casa sem Fim")
+    time.sleep(2)
+    print("2014")
+    time.sleep(2)
+    print("\nMeu amigo que não via online a semanas, me manda uma mensagem")
+    time.sleep(4)
+    print("\nBruno, cara, nós precisamos conversar!")
+    time.sleep(4)
+    print("\nFoi quando ele me disse sobre a Casa sem Fim")
+    time.sleep(4)
+    print("\nEla tinha esse nome pois ninguém nunca alcançou a saída final")
+    time.sleep(4)
+    print("\nAs regras eram simples: chegue na saída final e você ganha 5000 reais, nove cômodos no total")
+    time.sleep(5)
+    print("\nEu não acreditei nele. Por que eu deveria?")
+    time.sleep(4)
+    print("\nEu disse a ele que iria checar isso na outra noite")
+    time.sleep(4)
+    print("\n5000 reais soava bom demais pra ser verdade")
+    time.sleep(4)
+    print("\nNa noite seguinte...")
+    time.sleep(3)
+    print("\nQuando eu cheguei, imediatamente notei algo estranho sobre a casa")
+    time.sleep(5)
+    print("\nVocê já viu ou leu algo que não deveria te assustar, mas por alguma razão te gelava a espinha?")
+    time.sleep(6)
+    print("\nEntão pensei novamente...")
+    time.sleep(3)
+
+    play.entrada_casa()
+
+
+def quarto1():
+    time.sleep(3)
+    print("\nEu andei através da casa e o sentimento de mal estar apenas aumentou quando eu abri a porta da frente")
+    time.sleep(5)
+    print("\nMeu coração desacelerou e soltei um suspiro aliviado assim que entrei")
+    time.sleep(4)
+    print("\nO cômodo parecia como uma entrada de um hotel normal decorada para o Halloween")
+    time.sleep(5)
+    print("\nUm sinal foi colocado no lugar onde deveria ter um funcionário")
+    time.sleep(4)
+    print("\nSe lia: você já está no primeiro comodo. Mais oito a seguir")
+    time.sleep(4)
+    print("\nAlcance o final e você vence!")
+    time.sleep(4)
+    print("\nEu ri e fui para a primeira porta")
+    time.sleep(4)
+
+
+def volta_casa():
+    time.sleep(3)
+    print("\nDecidi voltar para o meu carro e retornar para minha casa")
+    time.sleep(4)
+    print("\nQuem daria tanto dinheiro para qualquer um chegar ao final de uma casa?!")
+    time.sleep(4)
+    print("\nSó poderia ter alguma pegadinha")
+    time.sleep(4)
+    print("\nChegando em casa...")
+    time.sleep(3)
+    print("\nPensei muito no caminho, quantidade de dinheiro seria de grande ajuda")
+    time.sleep(4)
+    print("\nNão que eu iria ficar rico com essa quantia")
+    time.sleep(4)
+    print("\nMas fora isso, se fosse alguma pegadinha")
+    time.sleep(4)
+    print("\nAo menos poderia parar de pensar que eu realmente poderia ganhar essa quantia em apenas alguns minutos")
+    time.sleep(4)
+
+    play.decisao_casa()
+
+
+def dormir():
+    time.sleep(3)
+    print("\nNo dia seguinte...")
+    time.sleep(4)
+    print("\nDecidir chamar o meu amigo e apenas vi que nem ao menos ele recebeu minhas mensagens")
+    time.sleep(4)
+    print("\nIsso durou dias, então resolvi esquece-lo")
+    time.sleep(4)
+    print("\nSimplesmente ele voltou do nada falando sobre a casa e sumiu")
+    time.sleep(4)
+    print("\nComo de costume...")
+    time.sleep(5)
+
+    play.continuar()
+
+
+if __name__ == "__main__":
+    introducao()
